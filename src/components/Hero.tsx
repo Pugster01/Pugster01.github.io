5ffbaa3b4e1df2740e4b9157d605137e25ac02c2
Pugster01 <rubpuga@gmail.com>
2024-06-28 13:34:03 -0500

    Added necessary assets for parallax

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,25 @@
 function Hero() {
     return (
-        <>
-            <h1>Hero</h1>
-            <p>Welcome! Here I'll show my skillset, experience, projects, and contact information</p>
-        </>
-    )
+		<>
+			<div className="w-full h-screen content-center">
+				<div className="grid relative parallax">
+					<div className="text-center content-center title-and-name">
+						<h1 className="text-4xl font-bold sm:text-6xl">Ruben Puga</h1>
+						<p>Frontend Developer - Fullstack Engineer</p>
+					</div>
+					<img id="svg-ground-1" src="src/assets/parallax/ground-1.svg"></img>
+					<img id="svg-ground-2" src="src/assets/parallax/ground-2.svg"></img>
+					<img id="svg-ground-3" src="src/assets/parallax/ground-3.svg"></img>
+					<img id="svg-mountains-1" src="src/assets/parallax/mountains-1.svg"></img>
+					<img id="svg-mountains-2" src="src/assets/parallax/mountains-2.svg"></img>
+					<img id="svg-stars" src="src/assets/parallax/stars.svg"></img>
+					{/* These will be used in a latter implementation
+					 <img id="svg-falling-star-1" src="src/assets/parallax/falling-star-1.svg"></img>
+					<img id="svg-falling-star-2" src="src/assets/parallax/falling-star-2.svg"></img> */}
+				</div>
+			</div>
+		</>
+    );
 }
 
 export default Hero
