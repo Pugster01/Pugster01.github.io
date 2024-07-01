710287876659965feaf982f912e5e33ce65ca038
Pugster01 <rubpuga@gmail.com>
2024-06-30 21:27:58 -0500

    Added card slide-in animation

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 interface ProjectCardProps {
     reverse: boolean;
@@ -9,15 +9,39 @@ interface ProjectCardProps {
     length: number;
 }
 
-function ProjectCard(props: ProjectCardProps ) {
-    const [index] = useState(0)   
+function ProjectCard(props: ProjectCardProps) {
+    const [index] = useState(0);
+    const [isIntersecting, setIsIntersecting] = useState(false);
+    const containerRef = useRef(null)
 
     const mappedTech = useMemo(() => props.techStack.map((t, index) => <img src={t} key={index} className="size-10"></img>), [])
 
+    // Effect for animation
+    useEffect(() => {
+        const observer = new IntersectionObserver(([entry]) => {
+            setIsIntersecting(entry.isIntersecting);
+            if(entry.isIntersecting) observer.disconnect();
+        }, {
+            root: null,
+            rootMargin: "0px",
+            threshold: 0.5
+        });
+        if(containerRef.current) {
+            observer.observe(containerRef.current);
+        }
+
+        return () => observer.disconnect();
+    }, [containerRef])
+    
+
     return (
-        <div className={"project-card p-10 rounded-xl flex gap-10 " + (props.reverse ? "flex-row-reverse" : "flex-row")}>
-            <img src={props.images[index]} className="h-96 rounded-lg"></img>
-            <div className="content-center flex flex-col justify-around">
+        <div 
+            className={"project-card p-10 rounded-xl flex gap-10 flex-wrap justify-center " 
+                + (props.reverse ? "flex-row-reverse " : "flex-row ") 
+                + (isIntersecting ? "animate-card" : "")}
+            ref={containerRef}>
+            <img src={props.images[index]} className="h-80 rounded-lg"></img>
+            <div className="content-center flex flex-col flex-auto w-64 justify-around">
                 <div>
                     <h1 className="text-2xl font-bold mb-2">{props.title}</h1>
                     <p>{props.text[index]}</p>
